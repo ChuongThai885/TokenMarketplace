09c4f3e1fb4eee9a39deaa62ab6ed6cea616c234
Chuong Thai <chuongthai885@gmail.com>
2023-06-17 18:25:06 +0700

    add page not found

--- src/pages/NoMatch/index.jsx
+++ src/pages/NoMatch/index.jsx
@@ -1,5 +1,24 @@
 import React from "react"
+import { ReactComponent as LogoIcon } from "../../assets/logo.svg"
+import { NavLink } from "react-router-dom"
 
 export const NoMatch = () => {
-    return <>404 Not found</>
+    return (
+        <div className="flex w-full h-full min-h-screen justify-center items-center">
+            <div className="flex flex-col items-center w-1/4">
+                <LogoIcon className="w-8 h-8 mb-6" />
+                <label className="text-xl font-bold mb-4">Page not found</label>
+                <label className="text-center text-sm text-gray-500 mb-6">
+                    The page could not be found. Click the button below to go
+                    back to your dashboard.
+                </label>
+                <NavLink
+                    to="/"
+                    className="bg-default-dark-blue/75 hover:bg-default-dark-blue text-white font-bold text-sm px-6 py-3 rounded-lg"
+                >
+                    Go to Dashboard
+                </NavLink>
+            </div>
+        </div>
+    )
 }
